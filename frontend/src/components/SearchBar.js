16f31c5f66e104882b208927146cdfda63254c11
phoenixdev-512 <abhaybs1729@gmail.com>
2025-08-04 21:32:11 +0530

    feat: implement initial SearchBar component with state for location and specialty

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function SearchBar() {
+    const [location, setLocation] = useState('');
+    const [specialy, setSpeciality]
+}
